97561fa768f2e49990db9308e9ba369ac84776c3
yoyo@home <1197993367@qq.com>
2017-08-17 11:24:02 +0800

    完成html_downloader下载器

--- baike_spider/html_downloader.py
+++ baike_spider/html_downloader.py
@@ -2,6 +2,21 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2017-08-17 10:23
 # @Author  : YouSheng
+import urllib2
 class HtmlDownloader(object):
-    def download(self, newUrl):
-        pass
+    def download(self, url):
+        req = urllib2.Request(url)
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError as e:
+            if hasattr(e, 'reason'):
+                print 'We failed to reach a server.'
+                print 'Reason: ', e.reason
+            elif hasattr(e, 'code'):
+                print 'The server couldn\'t fulfill the request.'
+                print 'Error code: ', e.code
+        else:
+            return response.read()
+
+
+
