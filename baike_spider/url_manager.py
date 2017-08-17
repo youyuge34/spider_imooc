aa2e29cb0d74d564269affcfbc86bf96181bba89
yoyo@home <1197993367@qq.com>
2017-08-17 11:09:21 +0800

    完成URL管理器

--- baike_spider/url_manager.py
+++ baike_spider/url_manager.py
@@ -3,14 +3,29 @@
 # @Time    : 2017-08-17 10:23
 # @Author  : YouSheng
 class UrlManager(object):
-    def addNewUrl(self, root_url):
-        pass
+    """
+    Url管理器，维护两个url的列表
+    """
+    def __init__(self):
+        self.newUrls = set()  # 待爬取的列表
+        self.oldUrls = set()  # 爬取过的列表
 
-    def hasNewUrl(self):
-        pass
-
-    def getNewUrl(self):
-        pass
+    def addNewUrl(self, url):
+        if url is None or url == '':
+            return
+        if url not in self.newUrls and url not in self.oldUrls:
+            self.newUrls.add(url)
 
     def addNewUrls(self, parsedUrls):
-        pass
+        if parsedUrls is None or len(parsedUrls) == 0:
+            return
+        for url in parsedUrls:
+            self.addNewUrl(url)
+
+    def hasNewUrl(self):
+        return len(self.newUrls) != 0
+
+    def getNewUrl(self):
+        newUrl = self.newUrls.pop()
+        self.oldUrls.add(newUrl)
+        return newUrl
