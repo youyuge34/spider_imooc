1a8aeb3cdb76474bbf5e08c028ddc67ce4bb8ac4
yoyo@home <1197993367@qq.com>
2017-08-17 14:04:20 +0800

    完成html_outputer 输出器

--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -3,8 +3,32 @@
 # @Time    : 2017-08-17 10:23
 # @Author  : YouSheng
 class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
     def collectData(self, parsedData):
-        pass
+        if (parsedData is not None):
+            self.datas.append(parsedData)
 
     def output(self):
-        pass
+        try:
+            fout = open('output.html', 'w')
+            fout.write('<html>')
+            fout.write('<body>')
+            # 数据集输出成表格形式
+            fout.write('<table>')
+
+            for data in self.datas:
+                fout.write('<tr>')
+                fout.write('<td>%s</td>' % data['url'].encode('utf-8'))
+                fout.write('<td>%s</td>' % data['title'].encode('utf-8'))
+                fout.write('<td>%s</td>' % data['summary'].encode('utf-8'))
+                fout.write('</tr>')
+
+            fout.write('</table>')
+            fout.write('</body>')
+            fout.write('</html>')
+            fout.close()
+        except IOError as e:
+            print e.message
+
